eaf714709469b166cea954600ec3fc03089c273a
Vasudevan Visvanathan <vasudevan.visvanathan@gmail.com>
2020-11-19 20:35:21 +0530

    Add files via upload

---
+++ telnum.py
@@ -0,0 +1,15 @@
+import random
+ccode = '+91'
+gen_random_first = '9'
+file1 = open('telnum.csv','w')
+#gen_random_second = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 9)])
+for i in range(0,1000):
+    gen_random_second = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 9)])
+    tel_num = ccode+'-'+ str(gen_random_first)+str(gen_random_second)
+    file1.write(tel_num)
+    file1.write('\n')
+    if i == 500:
+        ccode = '+92'
+        gen_random_first = '4'
+    print(tel_num)
+file1.close()
