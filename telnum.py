ac60f68e8000103ae6c7b88b46a31e75d25e944d
Vasudevan Visvanathan <vasudevan.visvanathan@gmail.com>
2020-11-19 20:49:44 +0530

    Update telnum.py

--- telnum.py
+++ telnum.py
@@ -4,7 +4,7 @@ gen_random_first = '9'
 file1 = open('telnum.csv','w')
 for i in range(0,1000):
     gen_random_second = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 9)])
-    tel_num = ccode+'-'+ str(gen_random_first)+str(gen_random_second)
+    tel_num = ccode+'-'+ gen_random_first+str(gen_random_second)
     file1.write(tel_num)
     file1.write('\n')
     if i == 500:
